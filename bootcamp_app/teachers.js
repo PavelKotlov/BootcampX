c60269d428a7f82be84f6147d36b1d0f82b95edf
PavelKotlov <pavelkotlov@gmail.com>
2023-03-09 00:52:52 -0800

    Setup postgres node client in bootcamp.app to run queries in CLI

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT 
-  teachers.name AS teacher,
-  cohorts.name AS cohort
-FROM teachers
-  JOIN assistance_requests ON teacher_id = teachers.id
-  JOIN students ON student_id = students.id
-  JOIN cohorts ON cohort_id = cohorts.id WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+const queryString = `
+SELECT teachers.name AS teacher, cohorts.name AS cohort
+FROM teachers 
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id WHERE cohorts.name LIKE $1
 GROUP BY cohort, teacher
-ORDER BY teacher;
-`)
+ORDER BY teacher`;
+
+const values = [`%${process.argv[2]}%`];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
